bd01369368e794078f93e05e2d1e5cabe6f5b54d
comrern <kb22541@bristol.ac.uk>
2025-07-31 13:57:35 +0100

    refactoring to extract local GWAS info

--- run_GWAS_comparisons.R
+++ run_GWAS_comparisons.R
@@ -17,6 +17,10 @@
 library(CAMERA)
 library(data.table)
 library(TwoSampleMR)
+library(ieugwasr)
+library(dplyr)
+library(gwasvcf)
+library(VariantAnnotation)
 
 setwd("../../data/")
 token <- readLines("token")[1]
@@ -115,22 +119,141 @@ heterogeneity <- function(data){
 }
 
 
+get_instruments <- function(GWAS_1, GWAS_2){
+  
+  g1 <- readVcf(paste0("vcfs/",ids_m[1], ".vcf.gz"))
+  g2 <- readVcf(paste0("vcfs/",ids_m[2], ".vcf.gz"))
+  
+  g1_tophits <- vcf_to_tibble(query_gwas(g1, pval = 5e-8))
+  g2_tophits <- vcf_to_tibble(query_gwas(g2, pval = 5e-8))
+  
+  g2_g1tophits <- vcf_to_tibble(query_gwas(g2, rsid = g1_tophits$rsid))
+  g1_g2tophits <- vcf_to_tibble(query_gwas(g1, rsid = g2_tophits$rsid))
+  
+  g1_merge <- rbind(g1_tophits, g1_g2tophits[g1_g2tophits$rsid %in% g1_tophits$rsid,])
+  g2_merge <- rbind(g2_tophits, g2_g1tophits[g2_g1tophits$rsid %in% g2_g1tophits$rsid,])
+  
+  
+  ##### extract regions for tophit SNPs in either gwas
+  regions <- paste0(g1_merge$seqnames, ":", g1_merge$start - 50000, "-", g1_merge$end + 50000)
+  
+  regions <- lapply(regions, function(r){
+    
+    a <- ieugwasr::associations(r, ids_m)
+    
+      if (inherits(a, "response")) {
+        message("Received a response object. Sleeping for 10 minutes and retrying...")
+        Sys.sleep(600)
+        a <- ieugwasr::associations(r, ids_m)
+      }
+    
+    # Only attempt to arrange if it's not a response object
+      if (!inherits(a, "response")) {
+        a <- a %>%
+          dplyr::arrange(position) %>%
+          dplyr::bind_rows()
+        
+        message(nrow(a))
+     
+        a <- lapply(ids_m, function(i) {
+          subset(a, id == i) %>%
+            dplyr::filter(!duplicated(rsid))
+        })
+        rsids <- Reduce(intersect, lapply(a, function(x) x$rsid))
+        a <- lapply(a, function(x) {
+          subset(x, rsid %in% rsids)
+        })
+        ea <- a[[1]]$ea
+        a <- lapply(a, function(x) {
+          index <- x$ea != ea
+          if (sum(index) > 0) {
+            x$beta[index] <- x$beta[index] * -1
+            nea <- x$nea[index]
+            x$nea[index] <- x$ea[index]
+            x$ea[index] <- x$nea[index]
+            x$eaf[index] <- 1 - x$eaf[index]
+            x <- subset(x, nea == a[[1]]$nea)
+          }
+          return(x)
+        })
+        rsids <- Reduce(intersect, lapply(a, function(x) x$rsid))
+        a <- lapply(a, function(x) {
+          subset(x, rsid %in% rsids) %>%
+            dplyr::arrange(chr, position)
+        })
+        
+      } else {
+        message("Second attempt also failed. Skipping this set.")
+        a <- NULL
+    }
+    return(a)
+    })
+}
+
+
+
 heterogeneity_calcs <- function(ids, method){
   
   if (method == "raw"){
-    suppressMessages(g1_tophits <- ieugwasr::tophits(ids[1], pval = 5e-5 , pop = "AMR" , opengwas_jwt = token))
-    suppressMessages(g2_tophits <- ieugwasr::tophits(ids[2], pval = 5e-5 , pop = "AMR", opengwas_jwt = token))
-    
-    suppressMessages(g2_g1tophits <- ieugwasr::associations(variants  = g1_tophits$rsid, id = ids[2], proxies = 0, opengwas_jwt = token))
-    suppressMessages(g1_g2tophits <- ieugwasr::associations(variants  = g2_tophits$rsid, id = ids[1], proxies = 0, opengwas_jwt = token))
-    
-    
-    g1xg2 <- heterogeneity(merge(g1_tophits, g2_g1tophits, by = "rsid"))
-    g2xg1 <- heterogeneity(merge(g2_tophits, g1_g2tophits, by = "rsid"))
+      suppressMessages(g1_tophits <- ieugwasr::tophits(ids[1], pval = 5e-6, pop = "AMR" , opengwas_jwt = token))
+      suppressMessages(g2_tophits <- ieugwasr::tophits(ids[2], pval = 5e-6,  pop = "AMR", opengwas_jwt = token))
       
-    df <- data.frame(rbind(g1xg2, g2xg1))
-    df[,1] <- ids
-  }  
+      
+      g1_chunks <- split(g1_tophits$rsid, ceiling(seq_along(g1_tophits$rsid) / 30))
+      g2_chunks <- split(g2_tophits$rsid, ceiling(seq_along(g2_tophits$rsid) / 30))
+      
+      # Initialize result container
+      results <- list()
+      
+      # Loop through each chunk
+      for (i in seq_along(g1_chunks)) {
+        chunk <- g1_chunks[[i]]
+        
+        # Try-catch to avoid API errors breaking the loop
+        try({
+          res <- extract_outcome_data(snps = chunk, outcomes = ids[2])
+          results[[i]] <- res
+        }, silent = TRUE)
+      }
+      
+      # Combine all results
+      g2_g1tophits <- bind_rows(results)
+      
+      
+      results <- list()
+      
+  
+      for (i in seq_along(g2_chunks)) {
+        chunk <- g2_chunks[[i]]
+        
+        try({
+          res <- extract_outcome_data(snps = chunk, outcomes = ids[1])
+          results[[i]] <- res
+        }, silent = TRUE)
+      }
+      
+      g1_g2tophits <- bind_rows(results)
+      
+      
+      g1_merge <- merge(g1_tophits, g2_g1tophits, by = "rsid")
+      g2_merge <- merge(g2_tophits, g1_g2tophits, by = "rsid")
+      
+      g1xg2 <- heterogeneity(g1_merge)
+      g2xg1 <- heterogeneity(g2_merge)
+        
+      df <- data.frame(rbind(g1xg2, g2xg1))
+      df[,1] <- ids
+      
+  }    
+  
+  
+  if (method == "fema"){
+    # FEMA_SNPS <- fixed_effects_meta_analysis_fast(
+    #                        as.matrix(g1_merge[,"beta.x","beta.y"])     
+                                      # )
+                            
+  }
+  
   return(df)
   
 }
@@ -182,18 +305,18 @@ for (current_trait in unique_traits)  {
   
   ids_m <-c(g_l[1,]$id, g_l[2,]$id)
   
+  
   # p <- pheno_harm(ids_m)
   # all_phen <- dplyr::bind_rows(all_phen, p)
+  
+  instruments <- get_instruments(ids_m)
+  
   
   h <- heterogeneity_calcs(ids_m, "raw")
   raw_het <- dplyr::bind_rows(raw_het, h)
   
   
-  
-  
-  
 }
-
 
 
 
