1b2a67266e0d688289e3d61acba42131c26c4e6b
comrern <kb22541@bristol.ac.uk>
2025-08-07 14:50:24 +0100

    local data reset

--- run_GWAS_comparisons.R
+++ run_GWAS_comparisons.R
@@ -125,92 +125,123 @@ get_instruments <- function(ids_f){
   g1_path <- paste0("vcfs/",ids_f[1], ".vcf.gz")
   g2_path <- paste0("vcfs/",ids_f[2], ".vcf.gz")
   
-  # get top htis
-  g1_tophits <- vcf_to_tibble(query_gwas(g1_path, pval=5e-8))
-  g2_tophits <- vcf_to_tibble(query_gwas(g2_path, pval=5e-8))
-
-  
-  
-  if (length(g1_tophits$rsid < 1)){
-    g1_tophits <- vcf_to_tibble(query_gwas(g1_path, pval=5e-7))
-    print("G1 has too few SNPs, reducing threshold")
-  }
-  if (length(g2_tophits$rsid < 1)){
-    g2_tophits <- vcf_to_tibble(query_gwas(g2_path, pval=5e-7))
-    print("G2 has too few SNPs, reducing threshold")
-  }
-  
-  
-  
-  g1_tophits$chrpos <- paste0(g1_tophits$seqnames, ":", g1_tophits$start, "-", g1_tophits$end )
-  g2_tophits$chrpos <- paste0(g2_tophits$seqnames, ":", g2_tophits$start, "-", g2_tophits$end )
-  
-  g2_g1tophits <- vcf_to_tibble(query_gwas(g2_path, chrompos = g1_tophits$chrpos))
-  g1_g2tophits <- vcf_to_tibble(query_gwas(g1_path, chrompos = g2_tophits$chrpos))
-  
-  g2_g1tophits$chrpos <- paste0(g2_g1tophits$seqnames, ":", g2_g1tophits$start, "-", g2_g1tophits$end )
-  g1_g2tophits$chrpos <- paste0(g1_g2tophits$seqnames, ":", g1_g2tophits$start, "-", g1_g2tophits$end )
-  
-  
-  g1_merge <- rbind(g1_tophits, g1_g2tophits[!g1_g2tophits$rsid %in% g1_tophits$rsid,])
-  g2_merge <- rbind(g2_tophits, g2_g1tophits[!g2_g1tophits$rsid %in% g2_tophits$rsid,])
-  
-  
-  ##### extract regions for tophit SNPs in either gwas
-  regions <- paste0(g1_merge$seqnames, ":", g1_merge$start - 50000, "-", g1_merge$end + 50000)
-  
-
-  write.table(as.data.frame(regions), "DEBUG_REGIONS_OUTPUT.txt", rownames=F)
-  
-  regions <- regions[!grepl("^X:", regions)]
-  
-  
-  regions <- lapply(regions, function(r){
+  # get top this
+  tryCatch({
     
-    a <- rbind(suppressMessages(vcf_to_tibble(query_gwas(g1_path, chrompos = r))),
-               suppressMessages(vcf_to_tibble(query_gwas(g2_path, chrompos = r))))  %>%
-      dplyr::arrange(start) %>%
-      dplyr::bind_rows()
-    message(nrow(a))
+    print(paste("Reading", g1_path))
+    g1_tophits <- suppressMessages(vcf_to_tibble(query_gwas(g1_path, pval=5e-8)))
+    print(paste("Done, Reading", g2_path))
+    g2_tophits <- suppressMessages(vcf_to_tibble(query_gwas(g2_path, pval=5e-8)))
     
-    a <- lapply(ids_f, function(i) {
-      subset(a, id == i) %>%
-        dplyr::filter(!duplicated(rsid))
-    })
-    rsids <- Reduce(intersect, lapply(a, function(x) x$rsid))
-    a <- lapply(a, function(x) {
-      subset(x, rsid %in% rsids)
-    })
-    ALT <- a[[1]]$ALT
-    a <- lapply(a, function(x) {
-      index <- x$ALT != ALT
-      if (sum(index) > 0) {
-        x$ES[index] <- x$ES[index] * -1
-        REF <- x$REF[index]
-        x$REF[index] <- x$ALT[index]
-        x$ALT[index] <- x$REF[index]
-        x$AF[index] <- 1 - x$AF[index]
-        x <- subset(x, REF == a[[1]]$REF)
-      }
-      return(x)
-    })
-    rsids <- Reduce(intersect, lapply(a, function(x) x$rsid))
-    a <- lapply(a, function(x) {
-      subset(x, rsid %in% rsids) %>%
-        dplyr::arrange(seqnames, start)
-    })
-    return(a)
+    if (length(g1_tophits$rsid )< 1){
+      g1_tophits <- vcf_to_tibble(query_gwas(g1_path, pval=5e-7))
+    }
+    if (length(g2_tophits$rsid) < 1){
+      g2_tophits <- vcf_to_tibble(query_gwas(g2_path, pval=5e-7))
+    }
 
+    g1_tophits$pval <- 10^(-g1_tophits$LP)
+    g2_tophits$pval <- 10^(-g2_tophits$LP)
+    
+    
+    ### clump hits
+    clump_list <- ld_clump(
+      tibble(rsid = g1_tophits$rsid , pval = g1_tophits$pval ),
+      plink_bin = get_plink_binary(),
+      bfile = "AMR"
+    )
+    g1_tophits <- g1_tophits[g1_tophits$rsid %in% clump_list$rsid, ]
+    
+    clump_list <- ld_clump(
+      tibble(rsid = g2_tophits$rsid , pval = g2_tophits$pval ),
+      plink_bin = get_plink_binary(),
+      bfile = "AMR"
+    )
+    g2_tophits <- g2_tophits[g2_tophits$rsid %in% clump_list$rsid, ]
+    
+    
+    # Step 2: Keep only rows where chr is between 1 and 22
+    g1_tophits <- g1_tophits[as.numeric(as.character(g1_tophits$seqnames)) %in% 1:22, ]
+    g2_tophits <- g2_tophits[as.numeric(as.character(g2_tophits$seqnames)) %in% 1:22, ]
+    
+    
+    
+    g1_tophits$chrpos <- paste0(g1_tophits$seqnames, ":", g1_tophits$start, "-", g1_tophits$end )
+    g2_tophits$chrpos <- paste0(g2_tophits$seqnames, ":", g2_tophits$start, "-", g2_tophits$end )
+    
+    g2_g1tophits <- vcf_to_tibble(query_gwas(g2_path, chrompos = g1_tophits$chrpos))
+    g1_g2tophits <- vcf_to_tibble(query_gwas(g1_path, chrompos = g2_tophits$chrpos))
+    
+    g2_g1tophits$chrpos <- paste0(g2_g1tophits$seqnames, ":", g2_g1tophits$start, "-", g2_g1tophits$end )
+    g1_g2tophits$chrpos <- paste0(g1_g2tophits$seqnames, ":", g1_g2tophits$start, "-", g1_g2tophits$end )
+    
+    
+    g1_merge <- rbind(g1_tophits[,c("chrpos", "ID", "id", "rsid","LP","ES","SE","REF", "ALT","seqnames","start","end")], 
+                      g1_g2tophits[!g1_g2tophits$rsid %in% g1_tophits$rsid, c("chrpos", "ID", "id", "rsid","LP","ES","SE","REF", "ALT","seqnames","start","end")])
+    g2_merge <- rbind(g2_tophits[,c("chrpos", "ID", "id", "rsid","LP","ES","SE","REF", "ALT","seqnames","start","end")], 
+                      g2_g1tophits[!g2_g1tophits$rsid %in% g2_tophits$rsid, c("chrpos", "ID", "id", "rsid","LP","ES","SE","REF", "ALT","seqnames","start","end")])
+    
+    write.table(g1_merge, "g1_merge") ## DBEUG
+    write.table(g2_merge, "g2_merge")
+    
+    ##### extract regions for tophit SNPs in either gwas
+    regions <- paste0(g1_merge$seqnames, ":", g1_merge$start - 50000, "-", g1_merge$end + 50000)
+    
+    
+    write.table(as.data.frame(regions), "DEBUG_REGIONS_OUTPUT.txt", row.names  =F)
+    
+    
+    regions <- lapply(regions, function(r){
+      
+      a <- rbind(suppressMessages(vcf_to_tibble(query_gwas(g1_path, chrompos = r))),
+                 suppressMessages(vcf_to_tibble(query_gwas(g2_path, chrompos = r))))  %>%
+        dplyr::arrange(start) %>%
+        dplyr::bind_rows()
+      message(nrow(a))
+      
+      a <- lapply(ids_f, function(i) {
+        subset(a, id == i) %>%
+          dplyr::filter(!duplicated(rsid))
+      })
+      rsids <- Reduce(intersect, lapply(a, function(x) x$rsid))
+      a <- lapply(a, function(x) {
+        subset(x, rsid %in% rsids)
+      })
+      ALT <- a[[1]]$ALT
+      a <- lapply(a, function(x) {
+        index <- x$ALT != ALT
+        if (sum(index) > 0) {
+          x$ES[index] <- x$ES[index] * -1
+          REF <- x$REF[index]
+          tmp <- x$REF[index]
+          x$REF[index] <- x$ALT[index]
+          x$ALT[index] <- tmp
+          x$AF[index] <- 1 - x$AF[index]
+          x <- subset(x, REF == a[[1]]$REF)
+        }
+        return(x)
+      })
+      rsids <- Reduce(intersect, lapply(a, function(x) x$rsid))
+      a <- lapply(a, function(x) {
+        subset(x, rsid %in% rsids) %>%
+          dplyr::arrange(seqnames, start)
+      })
+      return(a)
+      
     })
-  
-
-  return(list(g1_raw = g1_merge, g2_raw = g2_merge, regions ))
+    
+    return(list(g1_raw = g1_merge, g2_raw = g2_merge, regions = regions ))
+    
+  }, error = function(e) {
+    message("Error occurred: ", e$message, " Skipping current trait")
+    return(NULL)
+  })
   
 }
 
 run_fema <- function(betas, ses) {
   
-  if (length(betas >=1)){
+  if (nrow(betas) >=1){
   
   w <- 1 / ses^2
   beta <- rowSums(betas * w) / rowSums(w, na.rm=TRUE)
@@ -355,6 +386,7 @@ unique_traits <- unique(latAm_gwas$trait)
 
 all_phen <- data.frame()
 het <- data.frame()
+unique_traits <- unique_traits[5:9] ## DEBUG
 for (current_trait in unique_traits)  {
   
 
@@ -371,18 +403,21 @@ for (current_trait in unique_traits)  {
             
             instruments <- get_instruments(ids_m)
             
-            if (length(instruments$g1_raw$rsid) < 1 & length(instruments$g2_raw$rsid) < 1){
+            if (!is.null(instruments)) {
               
               h <- heterogeneity_calcs(instruments$g1_raw, instruments$g2_raw, "raw")
               het <- dplyr::bind_rows(het, h)
               
-              h <- heterogeneity_calcs(instruments[[3]], instruments[1:2] , "fema")
+              h <- heterogeneity_calcs(instruments[[3]], instruments[1:2], "fema")
               het <- dplyr::bind_rows(het, h)
               
-              write.table(all_phen, paste0(current_trait,"_pheno_results.txt"),quote = F, row.names = F)
-              write.table(het, paste0(current_trait,"_het_results.txt"),quote = F, row.names = F)
+              write.table(all_phen, paste0(current_trait, "_pheno_results.txt"), quote = FALSE, row.names = FALSE)
+              write.table(het, paste0(current_trait, "_het_results.txt"), quote = FALSE, row.names = FALSE)
+              
+            } else {
+              message("Too few SNPs for comparison.")
+            }
             
-              } else print("Too Few SNPs for comparison")
             
   } else { print("File(s) does not exists")}
 }
